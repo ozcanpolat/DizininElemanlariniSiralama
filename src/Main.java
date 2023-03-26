ecdbb0794eb49571f734453ee324645b884805af
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-03-27 01:32:38 +0300

    DizininElemanlariniSiralama

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count =0;
+        System.out.print("Dizinin boyutunu giriniz:");
+        int boyut=input.nextInt();
+        int[] list = new int[boyut];
+
+        for (int i=0;i<boyut;i++){
+            System.out.print((i+1)+".Elemanı giriniz:");
+            int eleman=input.nextInt();
+            list[count++]=eleman;
+        }
+        Arrays.sort(list);
+        System.out.print("Sıralama : ");
+        for(int j=0;j<list.length;j++){
+            System.out.print(list[j]+ " ");
+        }
+
+
+    }
+}
